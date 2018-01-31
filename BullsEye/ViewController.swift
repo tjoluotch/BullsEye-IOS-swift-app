00b1e3337e3b1b147191cf1cddbac208c1fc2235
TJ Oluotch <tjay@pc-199-176.customer.stjohn.liberty.lan>
2018-01-31 09:11:52 +0000

    added alert message to "Hit me!" button. Found out that apps are event driven

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -21,6 +21,15 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, World",
+                                      message: "This is my first app!",
+                                      preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
         
     }
 
